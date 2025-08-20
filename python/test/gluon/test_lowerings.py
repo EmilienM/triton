5d80174618421ebb64be76942c79ce7094a52145
Jokeren <robinho364@gmail.com>
2025-08-20 09:27:49 -0400

    Update

--- python/test/gluon/test_lowerings.py
+++ python/test/gluon/test_lowerings.py
@@ -69,7 +69,7 @@ def test_scan_layouts(M, N, src_layout, axis, sanitize_overflow, device):
         operand_index=0, k_width=2),
     ttgl.SliceLayout(
         dim=0, parent=ttgl.NVMMADistributedLayout(version=[2, 0], warps_per_cta=[4, 1, 1], ctas_per_cga=[1, 1, 1],  #
-                                                  cta_split_num=[1, 1, 1], cta_order=[2, 1, 0], instr_shape=[1, 16, 8])),
+                                                  cta_split_num=[1, 1, 1], cta_order=[2, 1, 0], instr_shape=[1, 16, 8 ])), #
     ttgl.SliceLayout(
         dim=1, parent=ttgl.DotOperandLayout(
             parent=ttgl.NVMMADistributedLayout(version=[2, 0], warps_per_cta=[4, 1, 1], ctas_per_cga=[1, 1, 1],  #
@@ -84,8 +84,8 @@ def test_scan_layouts(M, N, src_layout, axis, sanitize_overflow, device):
 @pytest.mark.parametrize("reduce_op", ["sum", "max"])
 def test_reduce_layouts(M, N, src_layout, axis, epilogue_kind, dtype_str, add_overflow_check, reduce_op, device):
     if src_layout == "linear_layout":
-        ttgl.DistributedLinearLayout(reg_bases=[[0, 16], [1, 0], [2, 0], [4, 0], [8, 0], [16, 0]], #
-                                     lane_bases=[[0, 0], [0, 1], [0, 2], [0, 4], [0, 8]], #
+        ttgl.DistributedLinearLayout(reg_bases=[[0, 16], [1, 0], [2, 0], [4, 0], [8, 0], [16, 0]],  #
+                                     lane_bases=[[0, 0], [0, 1], [0, 2], [0, 4], [0, 8]],  #
                                      warp_bases=[[32, 0], [0, 32]], block_bases=[], shape=[M, N])
 
     @gluon.jit
